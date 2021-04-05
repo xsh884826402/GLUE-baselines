e0d89fc921e6d06a19d1f94278364f1ddd1856e4
Shenghua Xu <shenghu@amazon.com>
2021-04-05 09:55:32 +0800

    test

--- src/preprocess.py
+++ src/preprocess.py
@@ -18,7 +18,7 @@ from tasks import CoLATask, MRPCTask, MultiNLITask, QQPTask, RTETask, \
 if "cs.nyu.edu" in os.uname()[1] or "dgx" in os.uname()[1]:
     PATH_PREFIX = '/misc/vlgscratch4/BowmanGroup/awang/'
 else:
-    PATH_PREFIX = '/home/ubuntu/User/xsh/GLUE-baselines/glue_data'
+    PATH_PREFIX = '/home/ubuntu/User/xsh/GLUE-baselines/glue_data/'
 # PATH_PREFIX = PATH_PREFIX + 'processed_data/mtl-sentence-representations/'
 
 #ALL_TASKS = ['mnli', 'mrpc', 'qqp', 'rte', 'qnli', 'snli', 'sst', 'sts-b', 'wnli', 'cola']
