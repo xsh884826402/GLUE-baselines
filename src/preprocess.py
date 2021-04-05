9bfa628cdca05a1e810c7f4e3e1af2255567ae00
Shenghua Xu <shenghu@amazon.com>
2021-04-05 09:48:03 +0800

    test

--- src/preprocess.py
+++ src/preprocess.py
@@ -22,6 +22,7 @@ else:
 PATH_PREFIX = PATH_PREFIX + 'processed_data/mtl-sentence-representations/'
 
 #ALL_TASKS = ['mnli', 'mrpc', 'qqp', 'rte', 'qnli', 'snli', 'sst', 'sts-b', 'wnli', 'cola']
+
 ALL_TASKS = ['mnli', 'mrpc', 'qqp', 'rte', 'qnliv2', 'snli', 'sst', 'sts-b', 'wnli', 'cola']
 NAME2INFO = {'sst': (SSTTask, 'SST-2/'),
              'cola': (CoLATask, 'CoLA/'),
